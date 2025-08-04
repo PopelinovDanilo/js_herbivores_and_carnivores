8248534654010ffdb83f74bfa5f2ac3123e830c1
PopelinovDanilo <popelinov.danilo@gmail.com>
2025-08-04 17:35:25 +0300

    fix

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,48 +1,33 @@
 'use strict';
-
 class Animal {
   static alive = [];
-
   constructor(name) {
     this.name = name;
     this.health = 100;
     Animal.alive.push(this);
   }
-
-  set health(value) {
-    this._health = value;
-
-    if (this._health <= 0) {
-      this._health = 0;
-      this.die();
-    }
+  die() {
+    Animal.alive = Animal.alive.filter((animal) => animal !== this);
   }
-
-  get health() {
-    return this._health;
-  }
-
-  die() {}
 }
-
 class Herbivore extends Animal {
   constructor(name) {
     super(name);
     this.hidden = false;
   }
-
   hide() {
     this.hidden = true;
   }
 }
-
 class Carnivore extends Animal {
   bite(target) {
     if (target instanceof Herbivore && !target.hidden) {
       target.health -= 50;
 
-      // 🔥 Ось тут фільтруємо масив живих після зміни health
-      Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+      if (target.health <= 0) {
+        target.health = 0;
+        target.die();
+      }
     }
   }
 }
